340f447eca81a46bed8128399a0573a6c4cd0017
Jane <rubencamcalderon@gmail.com>
2023-06-07 12:46:41 -0500

    rooms section created footer in progress first section slider finished look for errors on the future

---
+++ src/app.js
@@ -0,0 +1,27 @@
+var swiper_info = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+        delay: 5000,
+    }
+});
+
+var swiper_habitaciones = new Swiper(".mySwiper_2", {
+    slidesPerView: 4,
+    centeredSlides: true,
+    spaceBetween: 30,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
