aad557ac9b7d2d5e0e4bab9f55091f3bb23b4e1a
Jane <rubencamcalderon@gmail.com>
2023-06-14 12:00:49 -0500

    hamburger-menu finsihed

--- docs/app.js
+++ docs/app.js
@@ -28,3 +28,24 @@ var swiper_habitaciones = new Swiper(".mySwiper_2", {
 
 
 document.getElementById('video').play()
+
+const navbar__list = document.getElementById('navbar__list')
+
+var state = false
+
+document.getElementById('menu').addEventListener("click", () => {
+    if (state === false) {
+        navbar__list.style.transition = "0.4s"
+        navbar__list.style.visibility = "visible"
+        navbar__list.style.flexDirection = "column"
+        navbar__list.style.marginTop = "50px"
+        navbar__list.style.paddingBottom = "50px"
+        state = true
+    } else {
+        navbar__list.style.visibility = "hidden"
+        navbar__list.style.flexDirection = "row"
+        navbar__list.style.marginTop = "0"
+        navbar__list.style.paddingBottom = "0"
+        state = false
+    }
+})
